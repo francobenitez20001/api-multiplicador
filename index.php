355ca57d23453ff7d556a57c741641d17b9facff
Franco Benitez <franco_casla_benitez@hotmail.com>
2020-05-10 20:26:23 -0300

    conect to database

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-    $conexion = mysqli_connect('us-cdbr-east-06.cleardb.net','b32460f90c6ff5','ca902cc6','heroku_5a392985476cd37')or die(mysqli_error());
-    if ($conexion) {
-        echo 'conectado';
-    }else{
-        echo 'no';
-    }
+    require_once 'classes/Conexion.php';
+    $conexion = Conexion::conectar();
+    echo $conexion;
 ?>
