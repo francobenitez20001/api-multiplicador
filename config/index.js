39f78b3efb80efc2d853333ddf4628ef31621419
Franco Benitez <franco_casla_benitez@hotmail.com>
2020-06-04 13:47:45 -0300

    cors

--- config/index.js
+++ config/index.js
@@ -4,10 +4,20 @@ require('dotenv').config();
 const config = {
     dev: process.env.NODE_ENV !== 'production',
     port: process.env.PORT || 3000,
-    dbUser: process.env.DB_USER_DEV || 'b47fd523493b94',
-    dbPass: process.env.DB_PASSWORD_DEV || 'c40a31fa',
-    dbHost: process.env.DB_HOST_DEV || 'us-cdbr-east-05.cleardb.net',
-    dbName: process.env.DB_NAME_DEV || 'heroku_e6d840453fc91e4'
+    
+    dbUser: 'b47fd523493b94',
+    dbPass: 'c40a31fa',
+    dbHost: 'us-cdbr-east-05.cleardb.net',
+    dbName: 'heroku_e6d840453fc91e4'
 };
+// const config = {
+//     dev: process.env.NODE_ENV !== 'production',
+//     port: process.env.PORT || 3000,
+//     dbUser: process.env.DB_USER_DEV || 'b47fd523493b94',
+//     dbPass: process.env.DB_PASSWORD_DEV || 'c40a31fa',
+//     dbHost: process.env.DB_HOST_DEV || 'us-cdbr-east-05.cleardb.net',
+//     dbName: process.env.DB_NAME_DEV || 'heroku_e6d840453fc91e4' 
+
+// };
 
 module.exports = {config};
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 
+app.use(cors());
 
 const {config} = require('./config/index');
 
